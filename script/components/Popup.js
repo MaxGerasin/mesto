82464a26a1fcdd0886bc26d1f3032d4cfe390fe3
Maxim <maxgerasin32@gmail.com>
2023-04-19 16:56:36 +0300

    Popup

---
+++ script/components/Popup.js
@@ -0,0 +1,36 @@
+class Popup {
+  constructor(popupId) {
+    this._popup = document.querySelector(popupId);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  };
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  };
+
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close();
+    }
+  };
+
+  _isOverlay(evt) {
+    if (evt.target.classList.contains('popup')) {
+      this.close();
+    }
+  };
+
+  setEventListeners() {
+    const button = this._popup.querySelector('.popup__close');
+    button.addEventListener('click', this.close.bind(this));
+    this._popup.addEventListener('click', this._isOverlay.bind(this));
+  };
+}
+
+export default  Popup;
