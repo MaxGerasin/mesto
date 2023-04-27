d102dcb209640688f791c98cc2ea89dc4fb20e29
Maxim <maxgerasin32@gmail.com>
2023-04-27 18:31:00 +0300

    PopupAvatar

--- src/script/components/Section.js
+++ src/script/components/Section.js
@@ -1,12 +1,11 @@
 export default class Section {
-  constructor({ items, renderer }, selectorContainer) {
-    this._items = items;
+  constructor(renderer, selectorContainer) {
     this._renderer = renderer;
     this._container = document.querySelector(selectorContainer);
   }
 
-  render() {
-    this._items.forEach((item) => this._renderer(item));
+  render(items) {
+    items.forEach((item) => this._renderer(item));
   };
 
   addItem(element) {
